ad23b8869a54843e7e609c18ff766619bb56082d
Limon Islam <limon4u714@gmail.com>
2023-11-17 13:58:50 +0600

    Livewire form validation on Real time

--- Assignment-7/ostad/database/seeders/DatabaseSeeder.php
+++ Assignment-7/ostad/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Post;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,6 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Post::truncate();
+        Post::insert([
+            "name"=> "Rahim",
+            "email"=> "rahim@gmail.com",
+            "password"=> bcrypt("12345678"),
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
